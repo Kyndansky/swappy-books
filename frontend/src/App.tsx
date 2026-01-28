b54c86d4bf20cb99121810662e5f9b222ac99ff6
Davide_Riccobene <davidericcobene10@gmail.com>
2026-01-28 13:48:01 +0100

    added approuter file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Button } from "@material-tailwind/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState<number>(0);
 
+  useEffect(() => {
+    console.log("la pagina e' stata caricata");
+  }, []);
+  useEffect(() => {
+    console.log("counter eà cambiato");
+  }, [counter]);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Counter: </div>
+      {counter}
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        cliccami
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/AppRouter.tsx
@@ -0,0 +1,8 @@
+import { Route, Router, Routes } from "react-router-dom";
+import App from "./App";
+
+export function AppRouter(){
+    <Routes>
+        <Route />
+    </Routes>
+}
