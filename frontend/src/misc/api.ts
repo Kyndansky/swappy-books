03860d44b2d175e186c3a146778273b88462578d
Davide_Riccobene <davidericcobene10@gmail.com>
2026-02-18 13:27:28 +0100

    (frontend) added axios library for api calls and added a couple of lines for axios import and usage in api.ts

--- frontend/src/misc/api.ts
+++ frontend/src/misc/api.ts
@@ -1,4 +1,10 @@
+import axios from "axios";
+
+const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
+const api=axios.create({baseURL:BACKEND_API_URL});
+const apiAuth=axios.create({baseURL:BACKEND_API_URL});
 export async function getAuthenticationInfo():Promise<SwappyBooksResponse>{
+    
     const response:SwappyBooksResponse={
         successful:false,
         message:"Successfully logged in",
