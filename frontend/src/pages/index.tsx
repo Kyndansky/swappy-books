8530134359bdbf1cbc92883b1ed044c952df012a
Kyndansky <davidericcobene10@gmail.com>
2026-02-23 21:46:58 +0100

    changed docker commands in readme (i know, again) so that the frontend is actually updated when saving files (it didn't work before) (i used docker compose which simplifies things) and added docker-compose.yml, a docker file that specifies docker settings such as volumes etc..

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import DefaultLayout from "@/layouts/default";
 export default function LandingPage() {
   return (
     <DefaultLayout>
-      This is a simple home page example
+      This is a simple home page
     </DefaultLayout>
   );
 }
