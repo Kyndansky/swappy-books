0c08b7d8c07718b71c736d244e9fefc250cff22e
Davide_Riccobene <davidericcobene10@gmail.com>
2026-03-04 13:00:16 +0100

    fixed install.sql error and added swaps.tsx page file

--- frontend/src/misc/api.ts
+++ frontend/src/misc/api.ts
@@ -8,7 +8,6 @@ export async function getAuthenticationInfo():Promise<SwappyBooksResponse>{
     const response:SwappyBooksResponse={
         successful:false,
         message:"Successfully logged in",
-        username:"Sigma"
     };
     return response;
 }
---
+++ frontend/src/pages/swaps.tsx
@@ -0,0 +1,12 @@
+
+import React, { useEffect, useState } from "react";
+
+export default function SwapsPage(){
+
+    
+   
+    return (
+        <React.Fragment>
+        </React.Fragment>
+    )
+}
